4ca0ceb86e29c255e3350d77f029d3723a279b4a
titicuevas <enrique.cuevas@iesdonana.org>
2022-02-09 16:25:28 +0100

    Desglose de componentes

--- app/Models/Especialista.php
+++ app/Models/Especialista.php
@@ -26,4 +26,8 @@ class Especialista extends Model
     {
         return $this->belongsToMany(Compania::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ resources/views/citas/create-confirmar.blade.php
@@ -0,0 +1,16 @@
+<x-create-citas>
+
+<h1>Confirmar la cita</h1>
+<div>
+    <form action="{{route('store-cita-confirmar',[$compania,$cita])}}" method="POST" >
+    @csrf
+    <button type="submit" class="px-4" py-2 mt-4 m-2 rounded bg-green-300>Confirmar
+
+    </button>
+
+    </form>
+</div>
+
+<a href="{{ route('crear-cita-fecha-hora',[$compania, $cita->especialista->especialidad,$cita->especialista]) }}" class="mt-4 text-blue-900 hover:underline">Volver</a>
+</x-create-citas>
+
